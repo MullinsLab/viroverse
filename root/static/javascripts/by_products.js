9d9d244cbfa7b35262caf5f48c1089593db3df32
j shagam <jshagam@uw.edu>
2019-03-21 10:59:24 -0700

    Better encapsulation and button label

--- root/static/javascripts/by_products.js
+++ root/static/javascripts/by_products.js
@@ -1,7 +1,7 @@
-toggleAllChromats = (function(){
+window.addEventListener("load", function() {
     var state = false;
 
-    return function() {
+    function toggleAllChromats() {
         state = !state;
         document.querySelectorAll('input.skip-chromats').forEach(function(c) {
             c.checked = state;
@@ -9,4 +9,7 @@ toggleAllChromats = (function(){
 
         return false;
     }
-})();
+    document.querySelectorAll('button.skip-chromats-toggle').forEach(function(b) {
+        b.addEventListener("click", toggleAllChromats);
+    });
+});
