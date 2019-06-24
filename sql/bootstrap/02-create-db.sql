76696e4b2dd5bdbaad1bf786cd03625b72f2ef8b
Evan Silberman <silby@uw.edu>
2019-06-24 10:04:45 -0700

    Enhance database bootstrapping script
    • Rewrite in Perl due to some added complexity. Has no non-core dependencies.
    
    • You can now specify database name and role names when running the
      script, which will be useful for us when we have multiple tenants on the
      same database host, as we shortly will.
    
    • In addition to skipping role creation, you may also skip database
      creation, since you might not have that privilege, and instead be given
      a role account with owner access to an already-existing database by an
      administrator.
    
    • Rename script to bootstrap-database since you no longer necessarily
      have to create the database

--- sql/bootstrap/02-create-db.sql
+++ sql/bootstrap/02-create-db.sql
@@ -3,17 +3,17 @@
 
 \set ON_ERROR_STOP on
 
-CREATE DATABASE viroverse
-    WITH OWNER vverse_admin
+CREATE DATABASE :db_name
+    WITH OWNER :owner
          ENCODING 'UTF-8'
          TEMPLATE template0;
 
 -- Switch to the new database
-\c viroverse
+\c :db_name
 
 -- The default public schema from template0 is owned by the default Pg
 -- superuser and we want to own it so our owner has full control after this.
-ALTER SCHEMA public OWNER TO vverse_admin;
+ALTER SCHEMA public OWNER TO :owner;
 
 -- This works iff we're the superuser or the owner of the public schema.
 -- Otherwise, it quietly does nothing!
